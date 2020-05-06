a2a5ef9b6b1d174ef1b312ea0a51ddfd17decb80
Tongchai Laoverakul <bird@pixalione.com>
2020-05-06 15:19:39 +0700

    Add babel & only load devtool on dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,44 @@
 const path = require('path');
 
-module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
-    devtool: 'eval-cheap-module-source-map',
-    devServer: {
-	contentBase: './dist',
-	open: true,
-	hot: true,
-	overlay: true,
-    },
-    output: {
-	filename: 'main.js',
-	path: path.resolve(__dirname, 'dist'),
-    },
+module.exports = (env, argv) => {
+    let confObj = {
+	mode: 'production',
+	entry: './src/index.js',
+	output: {
+	    filename: 'main.js',
+	    path: pathTo('dist'),
+	},
+	module: {
+	    rules: [
+		{
+		    test: /\.js$/,
+		    loader: 'babel-loader',
+		    include: [ pathTo('src') ],
+		},
+	    ],
+	},
+    };
+    if (
+	(env && env.mode === 'development')
+	|| (argv && argv.mode === 'development')
+    ) {
+	confObj = {
+	    ...confObj,
+	    devtool: 'inline-source-map',
+	    devServer: {
+		contentBase: './dist',
+		open: true,
+		hot: true,
+		overlay: {
+		    errors: true,
+		    warnings: true,
+		},
+	    },
+	};
+    }
+    return confObj;
 };
+
+function pathTo(str) {
+    return path.resolve(__dirname, str);
+}
